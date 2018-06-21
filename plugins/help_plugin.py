1ed474f887bd0fe5bd27d037a567714ea923ee42
Samuel Maddock <sammaddock@sky.com>
2018-06-21 13:32:17 +0100

    Finished info command (for now)

--- plugins/help_plugin.py
+++ plugins/help_plugin.py
@@ -10,7 +10,9 @@ class HelpPlugin(Plugin):
         embed = MessageEmbed()
         embed.title = "Zilean Bot Info"
         embed.set_author(name="Zilean", icon_url="https://i.imgur.com/JreyU9y.png", url="https://github.com/Samuel-Maddock/Zilean")
-        embed.description = "Test embed"
+        embed.description = "A discord bot that tracks time spent playing league and other statistics :hourglass_flowing_sand: https://github.com/Samuel-Maddock/Zilean"
+        embed.add_field(name="Developed using:", value="https://github.com/pseudonym117/Riot-Watcher https://github.com/b1naryth1ef/disco")
+        embed.add_field(name="Use ~help for a list of commands!", value=":wave:")
         embed.color = "444751"
         embed.timestamp = datetime.utcnow().isoformat()
         embed.set_footer(text="Bot Information")
