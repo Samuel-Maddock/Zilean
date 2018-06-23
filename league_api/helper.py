5ecd4761d07adde287e53191753815b14cdde412
Samuel Maddock <sammaddock@sky.com>
2018-06-23 23:41:17 +0100

    Added static methods
    + validation of regions
    + retrieve static champion data
    + check whether a summoner has a match history

--- league_api/helper.py
+++ league_api/helper.py
@@ -10,16 +10,40 @@ class LeagueHelper:
         watcher = RiotWatcher(data["riot_api_key"])
         self.watcher = watcher
 
-    def user_exists(self, summoner_name):
-        result = True
+    def user_exists(self, region, summoner_name):
         try:
-            a = self.watcher.summoner.by_name("EUW1", summoner_name)
+            a = self.watcher.summoner.by_name(region, summoner_name)
         except HTTPError as err:
             if err.response.status_code == 404:
-                result = False
-        return result
+                return False
+        return True
 
-    def get_champion_data(self):
+    def has_match_history(self, region, summoner_name):
+        try:
+            summoner = self.watcher.summoner.by_name(region, summoner_name)
+            history = self.watcher.match.matchlist_by_account(region, summoner["accountId"])
+        except HTTPError as err:
+            if err.response.status_code == 404:
+                return False
+        return True
+
+    @staticmethod
+    def get_champion_data():
         with open("league_api/static_data/champions.json") as data_file:
             data = json.load(data_file)
         return data
+
+    @staticmethod
+    def validate_region(region):
+        region = region.upper()
+        if region in ["EUW", "NA", "EUN", "JP", "LAN", "OCE", "TR", "RU"]:
+            region += "1"
+        elif region == "KR":
+            pass
+        elif region == "LAS":
+            region += "2"
+        elif region == "EU":
+            region = "EUW1"
+        else:
+            region = None
+        return region
